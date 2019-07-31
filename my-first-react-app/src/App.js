01df23f2b8ec64f852d5f161d8300cb1ffca1350
mtakil <takil.mt@gmail.com>
2019-07-31 12:00:54 +0300

    second commit

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        gdfgdfhd
+        gdfg12313fhd
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
